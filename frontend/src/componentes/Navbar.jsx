fdf855357c75daecd95dada6df4c4f99e7f88ca3
mts <mts@gmail.com>
2022-09-24 17:09:21 -0300

    nav 2.0

--- frontend/src/componentes/Navbar.jsx
+++ frontend/src/componentes/Navbar.jsx
@@ -3,12 +3,22 @@ import "./componentes.css";
 // router-dom
 import { Link } from "react-router-dom";
 
+import { FaUser, FaHome } from "react-icons/fa";
+
 const Navbar = () => {
   return (
-    <nav className="nav-app d-flex align-items-center justify-content-center position-fixed">
-      <Link to="/">
+    <nav className="nav-app d-flex align-items-center justify-content-between position-fixed">
+      <Link to="/" className="mx-3">
         <img alt="LOGO" src="assets/KEOPS-type.png" className="logo-app" />
       </Link>
+      <div className="d-flex">
+        <Link to="/" className="usuario">
+          <FaHome size={25}/>
+        </Link>
+        <Link to="/usuario" className="mx-5 usuario">
+          <FaUser size={23}/>
+        </Link>
+      </div>
     </nav>
   );
 }
